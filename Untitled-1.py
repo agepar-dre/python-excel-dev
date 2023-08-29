3f3f5e27591244983dcc1082e346640a54a7be95
Angelo Brocca <84419409+AngeloBrocca@users.noreply.github.com>
2023-08-29 14:00:26 -0300

    Update Untitled-1.py
    comentarios

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,6 +2,7 @@ import pandas as pd
 import openpyxl
 import re
 
+# Função para remover linhas específicas de um arquivo de texto
 def remove_lines(filename, target_line):
     with open(filename, 'r') as file:
         lines = file.readlines()
@@ -17,6 +18,7 @@ def remove_lines(filename, target_line):
                 file.write(lines[i])
                 i += 1
 
+# Função para verificar se uma string é uma fórmula Excel
 def verifica_formula(texto):
     if texto.startswith('='):
         if texto[1].isalpha():
@@ -26,22 +28,27 @@ def verifica_formula(texto):
     else:
         return False
 
+# Função para substituir vírgula por ponto e vírgula em uma string
 def substituir_virgula_por_ponto_virgula(texto):
     padrao = r'\((.*?)\)'  # Padrão para encontrar o texto entre parênteses
     resultado = re.sub(padrao, lambda match: match.group().replace(',', ';'), texto)
     return resultado
 
+# Função para substituir ponto por vírgula em uma string
 def substituir_ponto_por_virgula(texto):
     padrao = r'\d+\.\d+'
     resultado = re.sub(padrao, lambda match: match.group().replace('.', ','), texto)
     return resultado
 
+# Função para remover espaços em branco de uma string
 def remove_space(string):
     return "".join(string.split())
 
+# Função para remover números de uma string
 def remove_num(string):
     return re.sub(r'\d+', '', string)
 
+# Função para converter um número inteiro em letra de coluna Excel
 def int_to_col_letter(n):
     result = ''
     while n >= 0:
@@ -50,11 +57,13 @@ def int_to_col_letter(n):
         n -= 1
     return result
 
+# Função para trocar um valor por sua chave em um dicionário
 def trocar_por_chave(val, dct):
     for chave, valor in dct.items():
         if valor == val:
             return chave
 
+# Função para extrair linhas de um arquivo de texto com base em uma variável alvo
 def extract_lines(filename, target_variable):
     lines_to_extract = []
     extracting = False
@@ -70,45 +79,47 @@ def extract_lines(filename, target_variable):
 
     return lines_to_extract
 
+# Função para gerar um prompt com base em uma fórmula Excel
 def gerar_prompt(f):
-    prompt = 'faça uma função em python para a formula de excel: ' + f + '\n'
+    prompt = 'faça uma função em python para a fórmula de Excel: ' + f + '\n'
     prompt += 'insira uma documentação da função indicando a correspondência de cada variável, preservando os nomes originais escritos na tabela abaixo, conforme o exemplo:\n'
 
     for x,y in dict_coluna.items():
         if  x == f[0:f.find('=')] or re.search(f'\W{x.upper()}\W', f):
             prompt += x+ ' : ' + y + '\n'
     
-    prompt+= '''Exemplo:
+    prompt += '''Exemplo:
     def função(inputs):
-	    #Função que calcula a variável x
-	    #Inputs
-	    #x: coluna x
-	    #Outputs
-	    #a: coluna a
-	    #b: coluna b
+	    # Função que calcula a variável x
+	    # Inputs
+	    # x: coluna x
+	    # Outputs
+	    # a: coluna a
+	    # b: coluna b
 	    x = a + b
 	    return x
-        
-        '''
+    '''
 
     return prompt
 
-
+# Expressão regular para encontrar variáveis em maiúsculas, como A1, B2, etc.
 REGEX_VAR = '(?<![\'":])([A-Z]{1,3}[0-9]{1,7})(?![\'":])'
 
+# Caminho para o arquivo Excel e arquivo de texto de saída
 path = r'c:\Users\est.angelo\Documents\Teste_real.xlsx'
 path_txt = r"c:\Users\est.angelo\Documents\Teste.txt"
+
+# Lê o arquivo Excel e cria um DataFrame
 a = pd.read_excel(path, header=1)
 
-
-# Carregar o arquivo Excel
+# Carrega o arquivo Excel com openpyxl
 wb = openpyxl.load_workbook(filename=path)
 sheet_names = wb.sheetnames
 name = sheet_names[0]
 sheet_ranges = wb[name]
 vals = sheet_ranges.values
 
-# Criar o DataFrame a partir dos valores e usar a primeira linha como nomes das colunas
+# Cria um DataFrame a partir dos valores e usa a primeira linha como nomes das colunas
 df = pd.DataFrame(vals)
 df = df.dropna(axis=1, how='all')
 df.drop(index=0, axis=0, inplace=True)
@@ -116,9 +127,9 @@ df.columns = a.columns
 
 dict_coluna = {}
 
-# Abrir o arquivo de texto para escrita
+# Abre o arquivo de texto para escrita
 with open(path_txt, "w") as f:
-    f.write('Dicionario de dados\n')
+    f.write('Dicionário de dados\n')
     colunas_i = []
 
     for col_name, col_values in df.items():
@@ -132,16 +143,16 @@ with open(path_txt, "w") as f:
         f.write(f"{letra}: {coluna}\n")
     
     f.write('-' * 30 + '\n') 
-    f.write('Formulas:\n')
+    f.write('Fórmulas:\n')
 
-    # Iterar sobre as colunas e seus valores
+    # Itera sobre as colunas e seus valores
     for col_name, col_values in df.items():
         written = set()
         have_l = False
         col_name_print = trocar_por_chave(col_name, dict_coluna)
         f.write(f"Coluna {col_name_print} ({col_name}):\n")
 
-        # Iterar sobre os valores da coluna
+        # Itera sobre os valores da coluna
         for cell_value in col_values:
             
             if isinstance(cell_value, str):
@@ -167,11 +178,12 @@ with open(path_txt, "w") as f:
             f.write('Não há fórmulas nesta coluna.\n')
         f.write('-' * 30 + '\n')
 
-
+# Remove linhas desnecessárias do arquivo de texto
 remove_lines(path_txt, 'Não há fórmulas nesta coluna.')
 
-# Fechar o arquivo de texto
+# Fecha o arquivo de texto
 print("Processamento concluído e o resultado foi salvo em 'Teste.txt'.\n\n\n")
 
+# Imprime prompts gerados com base nas fórmulas encontradas
 for l in extract_lines(path_txt, 'eb'):
     print(gerar_prompt(l.strip()))
